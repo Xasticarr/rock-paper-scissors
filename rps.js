2d30ca13057d08e94a3d51daf50b656f07f62f3d
Xasticarr <sprexford@gmail.com>
2022-10-26 16:57:04 -0400

    Add prompt for player choice

--- rps.js
+++ rps.js
@@ -3,16 +3,26 @@ const choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice (choice) {
     let x = Math.random();
         if (x <= 0.34){
-            choice = ("Rock");
+            choice = ("ROCK");
         } else if (x <= 0.67) {
-            choice = ("Paper");
+            choice = ("PAPER");
         } else {
-            choice = ("Scissors");
+            choice = ("SCISSORS");
 
         }
     let text1 = ("Computer has chosen ");
-console.log(text1 + "'" + choice + "'");
+console.log(text1 + choice);
 
 }
 
-getComputerChoice("")
+//getComputerChoice("") //This is calling the function to show a console log with CPU Choice
+
+let playerChoice = prompt("Please choose Rock, Paper, or Scissors.");
+let playerSelection = playerChoice.toUpperCase();
+let text2 = ("You have chosen ");
+console.log(text2 + playerSelection);
+
+
+function playRPS (playerSelection, computerSelection) {
+
+}
